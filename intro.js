764c1dd64093094fa4c3cf6ee8c7ad2d61d5dc74
Elena Sontu <ella_west@icloud.com>
2019-03-12 12:21:08 +0200

    schimbare

--- intro.js
+++ intro.js
@@ -1,20 +1,3 @@
-console.info("my first js file")
-
-var name = "Elena";
-var employed = true;
-
-console.warn("employed");
-console.warn(employed);
-
-console.info("Name:");
-console.info(name);
-
-name = "Sontu";
-
-console.info("Name");
-console.info(name);
-
-
 function getWelcomeMsg() {
     return "Welcome to my site!";
 }
@@ -36,43 +19,7 @@ console.info("summaryElement", summaryElement);
 var color= getColor();
 summaryElement.style.color = color;
 
-// first example
-function hideHomePage() {
-    var el = document.getElementById('home-page');
-    el.style.display = 'none';
-}
-function showSkillsPage() {
-    var el = document.getElementById('skills-page');
-    el.style.display = 'block';
-}
 
-function clickOnSkills() {
-    // hideHomePage();
-     //showSkillsPage();
-     hidePage('home-page');
-     hidePage('languages-page');
-     hidePage('about-page');
-     showPage('skills-page');
- }
-function clickOnHome() {
-    showPage('home-page');
-    hidePage('languages-page');
-    hidePage('about-page');
-    hidePage('skills-page');
-}
-function clickOnLanguages() {
-    hidePage('home-page');
-    showPage('languages-page');
-    hidePage('about-page');
-    hidePage('skills-page');   
-}
-function clickOnAbout() {
-    hidePage('home-page');
-    hidePage('languages-page');
-    showPage('about-page');
-    hidePage('skills-page');   
-}
-// first example-end
 
 function hidePage(page) {
     var el = document.getElementById(page);
@@ -82,5 +29,6 @@ function showPage(page) {
     var el = document.getElementById(page);
     el.style.display = 'block';
 }
+
 
 
