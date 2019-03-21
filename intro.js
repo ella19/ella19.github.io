348de50c03fb6035434f328e84bd1b96ca3175c4
Elena Sontu <ella_west@icloud.com>
2019-03-22 01:46:01 +0200

    display skills from js in html

--- intro.js
+++ intro.js
@@ -56,10 +56,13 @@ initMenu();
 
 function showSkills() {
     var skills=['html', 'css','js'];
-    console.warn('showSkills', skills);
-    skills.forEach(function(skill, index) {
-        console.info("#" + (index + 1) + " " + skill);
-    });    
+    
+    var htmlSkills = skills.map(function(skill, index) {
+        return '<li>' + skill + '</li>';
+    }); 
+    
+    var ul = document.querySelector('#skills-page ul');
+    ul.innerHTML = htmlSkills.join(' ');
 }
 
 showSkills();
