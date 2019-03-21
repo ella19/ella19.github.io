bfccf26d952784aad82839c4b10dff64e7d48a71
Elena Sontu <ella_west@icloud.com>
2019-03-21 04:56:55 +0200

    extract printSkill

--- intro.js
+++ intro.js
@@ -58,8 +58,14 @@ function showSkills() {
     var skills=['html', 'css','js'];
     console.warn('showSkills', skills);
     for(var i = 0; i < skills.length; i++) {
-        console.info("#" + (i + 1) + " " + skills[i]);
+        printSkill(skills[i], i);
     }
+    
+    function printSkill(skill, index) {
+        console.info("#" + (index + 1) + " " + skill);
+    }
+
+    skills.forEach(printSkill);
 }
 
 showSkills();
