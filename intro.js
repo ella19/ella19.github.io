f3bddfeb0a38a3d77bc68e21d37894fc8e2c6c13
Elena Sontu <ella_west@icloud.com>
2019-03-22 00:50:30 +0200

    use skills.forEach

--- intro.js
+++ intro.js
@@ -57,17 +57,16 @@ initMenu();
 function showSkills() {
     var skills=['html', 'css','js'];
     console.warn('showSkills', skills);
-    for(var i = 0; i < skills.length; i++) {
-        printSkill(skills[i], i);
-    }
+    skills.forEach(printSkill);
     
     function printSkill(skill, index) {
         console.info("#" + (index + 1) + " " + skill);
     }
 
-    skills.forEach(printSkill);
 }
 
 showSkills();
+
+
 
 
