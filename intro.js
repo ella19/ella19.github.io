8bdb132712c5ddf51f08ebd44c15a8ce6ca621c0
Elena Sontu <ella_west@icloud.com>
2019-03-26 21:51:54 +0200

    improve using ternary if

--- intro.js
+++ intro.js
@@ -65,12 +65,7 @@ function showSkills() {
     ];
     
     var htmlSkills = skills.map(function(skill) {
-        var endorsedBy = skill.endorsedBy;
-        if (endorsedBy) {
-            endorsedBy = ' - ' + endorsedBy;
-        } else {
-            endorsedBy = '';
-        }
+        var endorsedBy = skill.endorsedBy ? ' - ' + skill.endorsedBy : '';
         var endorsements = ` <span class="endorsement">(${skill.endorsements}${endorsedBy})</span>`;
         return '<li>' + skill.name.toUpperCase() + endorsements + '</li>';
     }); 
